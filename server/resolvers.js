9236a1cba7b19b3da6e5120ccf9957aab67acad2
Andras Popovics <popovics@ndras.hu>
2023-02-06 20:14:36 +0100

    Update and delete

--- server/resolvers.js
+++ server/resolvers.js
@@ -15,11 +15,29 @@ export const resolvers = {
             
             return Job.create({...input, companyId: context.user.companyId});
         },
-        deleteJob: (_root, {id}) => {
+        deleteJob: async (_root, {id}, context) => {
+            if (!context.user) {
+                throw new Error("Unauthorized");
+            }
+
+            const {companyId} = await Job.findById(id);
+            if (companyId !== context.user.companyId) {
+                throw new Error("Unauthorized");
+            }
+
             return Job.delete(id);
         },
-        updateJob: (_root, {input}) => {
-            return Job.update(input);
+        updateJob: async (_root, {input}, context) => {
+            if (!context.user) {
+                throw new Error("Unauthorized");
+            }
+
+            const {companyId} = await Job.findById(input.id);
+            if (companyId !== context.user.companyId) {
+                throw new Error("Unauthorized");
+            }
+
+            return Job.update({...input, companyId: context.user.companyId});
         }
     },
 
